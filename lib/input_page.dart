a4e48b2f2ebfc7f3541d179061137d6df85d7f32
Poppy Sari <poppy.sari@rga.com>
2021-11-14 22:46:39 +0700

    :sparkles: navigate

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -165,11 +166,20 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            Container(
-              color: kBottomContainerColor,
-              margin: EdgeInsets.only(top: kSpacingSm),
-              width: double.infinity,
-              height: kBottomContainerHeight,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ResultPage()),
+                );
+              },
+              child: Container(
+                child: Text('CALCULATE'),
+                color: kBottomContainerColor,
+                margin: EdgeInsets.only(top: kSpacingSm),
+                width: double.infinity,
+                height: kBottomContainerHeight,
+              ),
             ),
           ],
         ),
---
+++ lib/result_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('RESULT'),
+        ),
+        body: Container(
+          child: Text('calculator result'),
+        ));
+  }
+}
